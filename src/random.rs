d54eaea1e0158bffda38df8e1e366b156d7e419d
Zack <zphixon@gmail.com>
2022-05-30 19:39:07 -0400

    add assert for number of errors

--- src/random.rs
+++ src/random.rs
@@ -1,7 +1,8 @@
 use std::io::Write;
 
-const NUM_ERRORS: usize = 5000;
+const NUM_ERRORS_PER_THREAD: usize = 5000;
 const NUM_THREADS: usize = 100;
+const TOTAL_ERRORS: usize = NUM_THREADS * NUM_ERRORS_PER_THREAD;
 const PROGRESS_WIDTH: usize = 60;
 
 error_report::make_reporter!(Idk);
@@ -13,7 +14,7 @@ fn main() {
     let (tx, rx) = flume::unbounded();
     let make_thread = |tx: Sender<()>| {
         return move || {
-            for i in 0..NUM_ERRORS {
+            for i in 0..NUM_ERRORS_PER_THREAD {
                 report!(format!("{i}"));
                 tx.send(()).unwrap();
             }
@@ -24,15 +25,17 @@ fn main() {
     for _ in 0..NUM_THREADS {
         threads.push(std::thread::spawn(make_thread(tx.clone())));
     }
+    drop(tx); // so that the loop can exit
 
-    let total_reports = NUM_ERRORS * NUM_THREADS;
     let mut num_reports = 0;
     let mut prev_num_chars = 0;
     loop {
-        rx.recv().unwrap();
+        if matches!(rx.recv(), Err(flume::RecvError::Disconnected)) {
+            break;
+        }
         num_reports += 1;
 
-        let ratio = (num_reports as f64) / (total_reports as f64);
+        let ratio = (num_reports as f64) / (TOTAL_ERRORS as f64);
         let num_chars = (PROGRESS_WIDTH as f64) * ratio;
         let num_chars_int = num_chars as usize;
         let bar_fill = "=".repeat(num_chars_int);
@@ -40,14 +43,10 @@ fn main() {
 
         if prev_num_chars != num_chars_int || num_reports == 1 {
             let percent = (ratio * 100.0) as usize;
-            print!("\r{num_reports}/{total_reports} {percent:3}% {bar}");
+            print!("\r{num_reports}/{TOTAL_ERRORS} {percent:3}% {bar}");
             std::io::stdout().flush().unwrap();
         }
         prev_num_chars = num_chars_int;
-
-        if num_reports == total_reports {
-            break;
-        }
     }
     println!();
 
@@ -55,7 +54,9 @@ fn main() {
         thread.join().unwrap();
     }
 
-    for (_, err) in et.done() {
+    let errs = et.done();
+    for (_, err) in errs.iter() {
         assert!(err.extra().is_none());
     }
+    assert_eq!(errs.len(), TOTAL_ERRORS);
 }
